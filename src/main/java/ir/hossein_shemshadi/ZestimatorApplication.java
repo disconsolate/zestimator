6f307bb4c991178782d9918517ff1ee8512e3fe6
disconsolate <hossein.shemshadi@gmail.com>
2018-10-26 10:04:51 +0330

    Initialize project

---
+++ src/main/java/ir/hossein_shemshadi/ZestimatorApplication.java
@@ -0,0 +1,11 @@
+package ir.hossein_shemshadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZestimatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZestimatorApplication.class, args);
+    }
+}
